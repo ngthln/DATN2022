d9d5815298c7bc8d0213eac9c3085ca6a114dc14
Nguyễn Thành Luân <ngthln1201@gmail.com>
2022-08-30 17:21:11 +0700

    create project

---
+++ DATN2022/DBContext/DATN2022DbContext.cs
@@ -0,0 +1,32 @@
+﻿using DATN2022.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DATN2022.DBContext
+{
+    public class DATN2022DbContext : DbContext
+    {
+        public DbSet<CoachOwner> CoachOwners { get; set; }
+        public DATN2022DbContext(DbContextOptions<DATN2022DbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CoachOwner>().HasData(new CoachOwner
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Uncle",
+                LastName = "Bob",
+                Email = "uncle.bob@gmail.com",
+                DateOfBirth = new DateTime(1979, 04, 25),
+                PhoneNumber = "999-888-7777"
+            }, new CoachOwner
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Vital",
+                LastName = "Water",
+                Email = "vital.bob@gmail.com",
+                DateOfBirth = new DateTime(1920, 05, 15),
+                PhoneNumber = "666-123-2345"
+            });
+        }
+    }
+}
---
+++ DATN2022/Models/CoachOwner.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace DATN2022.Models
+{
+    public class CoachOwner
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Nationality { get; set; }
+        public string LisenseNo { get; set; }
+        public string Address { get; set; }
+
+    }
+}
